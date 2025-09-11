985ac164ed22df9a323aea6df82b6ed7d4d2175f
JK <bowajk@gmail.com>
2025-09-11 19:05:01 -0300

    Fix file names

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -4,15 +4,27 @@ mod clipboard_list;
 #[cfg(any(feature = "core", feature = "ui", feature = "business-finance"))]
 #[doc(hidden)]
 pub use clipboard_list::*;
-#[cfg(feature = "uncategorized")]
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
 #[doc(hidden)]
 mod copy_bold;
-#[cfg(feature = "uncategorized")]
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
 #[doc(hidden)]
 pub use copy_bold::*;
-#[cfg(feature = "uncategorized")]
+#[cfg(any(feature = "core", feature = "flags", feature = "micro-bold"))]
 #[doc(hidden)]
 mod external_link_bold;
-#[cfg(feature = "uncategorized")]
+#[cfg(any(feature = "core", feature = "flags", feature = "micro-bold"))]
 #[doc(hidden)]
 pub use external_link_bold::*;
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
+#[doc(hidden)]
+mod gem_fill;
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
+#[doc(hidden)]
+pub use gem_fill::*;
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
+#[doc(hidden)]
+mod square_bars_fill;
+#[cfg(any(feature = "ui", feature = "micro-bold"))]
+#[doc(hidden)]
+pub use square_bars_fill::*;
