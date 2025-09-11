74f7c91c710b44c53c896972e0932d12d82590c9
JK <bowajk@gmail.com>
2025-09-11 11:27:07 -0300

    Add clipboard

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -1,3 +1,9 @@
+#[cfg(any(feature = "core", feature = "ui", feature = "business-finance"))]
+#[doc(hidden)]
+mod clipboard_list;
+#[cfg(any(feature = "core", feature = "ui", feature = "business-finance"))]
+#[doc(hidden)]
+pub use clipboard_list::*;
 #[cfg(feature = "uncategorized")]
 #[doc(hidden)]
 mod copy_bold;
